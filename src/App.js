fedfd079343f6b82c819fc053a3743230375766a
shin2012649 <shin2012649@gmail.com>
2023-10-05 21:35:49 +0900

    프론트엔드 CI/CD 테스트2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BleuAuction 화면 작업 중!.
+          BleuAuction 화면 작업 중!
         </p>
         <a
           className="App-link"
