ca62896dcf9710e90a4f93974d8d97663b23ff14
shin2012649 <shin2012649@gmail.com>
2023-10-06 21:36:18 +0900

    App.js 수정 (경로 변경) Axios Test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Notice from './Notice'; 
+import Notice from './components/Notice';
 
 function App() {
   return (
