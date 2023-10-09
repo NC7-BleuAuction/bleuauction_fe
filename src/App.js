0d5e141dd07cfec2de51f55cfc8f6a16e89ffcd3
shin2012649 <shin2012649@gmail.com>
2023-10-10 04:25:04 +0900

    nginx.conf 수정4 및 테스트

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
-import Notice from './components/Notice/Notice';
 
 function App() {
   return (
-    <Routes>
-      <div className="App">
-        <header className="App-header">
-          <h1>bleuauction 작업중</h1>
-          <Link to="/notices">Go to Notices</Link>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      <Routes>
-        <Route path="/notices" element={<Notice />} />
-      </Routes>
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          여기는 킹종원이 점령했다.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
