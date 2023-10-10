aaddcb1afbb375e48b061d7369d95e37037a27a0
shin2012649 <shin2012649@gmail.com>
2023-10-10 19:48:19 +0900

    코드 정리 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Test from './components/Test/Test'; // 경로 수정
-import Test2 from './components/test2'
+import Test2 from './components/Test/test2'
 import Main from './components/main'
 
 function App() {
