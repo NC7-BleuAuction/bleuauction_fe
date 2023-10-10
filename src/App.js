060c8efbe785fa00b7bc7bfe3bd15cc5b4947486
shin2012649 <shin2012649@gmail.com>
2023-10-10 15:50:30 +0900

    새로고침없이 값 가져오기 코드 원복

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/Test/Test';
+import Test from './components/Test/Test'; // 경로 수정
 
 function App() {
-  const location = useLocation();
-  const [showTestPage, setShowTestPage] = useState(true);
-
-  useEffect(() => {
-    // URL 경로가 "/api/test"인 경우 Test 컴포넌트를 렌더링
-    setShowTestPage(location.pathname === '/api/test');
-  }, [location.pathname]);
-
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>bleuauction 작업중!</p>
+          <p>
+            bleuauction 작업중!
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -30,8 +24,7 @@ function App() {
           <Link to="/api/test" className="App-link">Go to TestPage</Link>
         </header>
         <Routes>
-          {/* showTestPage가 true인 경우에만 Test 컴포넌트를 렌더링 */}
-          {showTestPage && <Route path="/api/test" element={<Test />} />}
+          <Route path="/api/test" element={<Test />} />
         </Routes>
       </div>
     </Router>
@@ -39,3 +32,4 @@ function App() {
 }
 
 export default App;
+
