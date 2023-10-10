f471494b8bc2529bc2d61904208be22f016f276b
shin2012649 <shin2012649@gmail.com>
2023-10-10 15:07:12 +0900

    새로고침없이 값 가져오기 Test1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useHistory } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/Test/Test'; // 경로 수정
 
 function App() {
+  const history = useHistory(); // useHistory를 사용하여 history 객체 가져오기
+
+  const goToTestPage = () => {
+    history.push('/api/test');
+  };
+
   return (
     <Router>
       <div className="App">
@@ -21,7 +27,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/api/test" className="App-link">Go to TestPage</Link>
+          <Link to="/api/test" className="App-link" onClick={goToTestPage}>Go to TestPage</Link>
         </header>
         <Routes>
           <Route path="/api/test" element={<Test />} />
