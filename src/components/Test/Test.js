bca08da03fa3741a538257cfb424fb626df2e38a
shin2012649 <shin2012649@gmail.com>
2023-10-10 20:14:11 +0900

    코드 정리 2 (Test.js)

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -3,82 +3,41 @@ import axios from 'axios';
 import './Test.css';
 
 const Test = () => {
-  const [testStatus, setState] = useState({
+  const [testStatus, setTestStatus] = useState({
     loading: false,
-    response: '값'
+    response: ''
   });
+
   useEffect(() => {
-    //hello()
-  })
+    hello();
+  }, []);
+
   const hello = () => {
     axios.get('/api/test')
-    .then((response) => {
-      console.log('Axios Response :' , response)
-      setState({ response: response.data, loading: false }); // 로딩 상태를 false로 변경
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-      setState({ response: '값',loading: false }); // 에러 발생 시 로딩 상태를 false로 변경
-    });
-  }
-  
+      .then((response) => {
+        console.log('Axios Response:', response);
+        setTestStatus({ response: response.data, loading: false });
+      })
+      .catch((error) => {
+        console.error('데이터를 가져오는 중 오류 발생:', error);
+        setTestStatus({ response: '데이터를 가져오는 중 오류 발생', loading: false });
+      });
+  };
 
-  console.log("testStatus : ",testStatus)
   return (
     <>
-      <h1>Test API</h1>
+      <h1>테스트 API</h1>
       {testStatus.loading ? (
-        <p>Loading...</p> // 로딩
+        <p>로딩 중...</p>
       ) : (
         <div className="response-container">
-          <p>API Response:</p>
+          <p>API 응답:</p>
           <p className="response-text">{testStatus.response}</p>
           <p>짜잔! 성공적으로 값을 가져왔습니다.</p>
         </div>
       )}
     </>
   );
-}
-
-// class Test extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       response: '',
-//       loading: true, // 로딩 상태
-//     };
-//   }
-
-//   componentDidMount() {
-//     // `/api/test` 엔드포인트로 GET 요청
-//     axios.get('/api/test')
-//       .then((response) => {
-//         this.setState({ response: response.data, loading: false }); // 로딩 상태를 false로 변경
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching data:', error);
-//         this.setState({ loading: false }); // 에러 발생 시 로딩 상태를 false로 변경
-//       });
-//   }
-
-//   render() {
-//     const { response, loading } = this.state;
-
-//     return (
-//       <div className="test-container">
-//         <h1>Test API</h1>
-//         {loading ? (
-//           <p>Loading...</p> // 로딩
-//         ) : (
-//           <div className="response-container">
-//             <p>API Response:</p>
-//             <p className="response-text">{response}</p>
-//             <p>짜잔! 성공적으로 값을 가져왔습니다.</p>
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
+};
 
 export default Test;
