d3d1f1853549c962bea84f109250a6362a012754
shin2012649 <shin2012649@gmail.com>
2023-10-10 09:55:13 +0900

    App.js 원복

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          여기는 킹종원이 점령했다.
+          bleuauction 작업중!
         </p>
         <a
           className="App-link"
