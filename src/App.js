4d86c2c2286a32fe50c1a8dda318e18ccb441278
shin2012649 <shin2012649@gmail.com>
2023-10-10 12:03:17 +0900

    라우트 Test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test/Test'; // 경로 수정
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          bleuauction 작업중!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            bleuauction 작업중!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
           </a>
-        <a
-          className="App-link"
-          href="https://bleuauction.co.kr/api/test"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to TestPage
-        </a>
-      </header>
-    </div>
+          <Link to="/api/test" className="App-link">Go to TestPage</Link>
+        </header>
+        <Routes>
+          <Route path="/api/test" element={<Test />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
