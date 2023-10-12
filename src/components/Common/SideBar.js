fa3a72b3e99490c4825d87189027e4e8f15a86a0
shin2012649 <shin2012649@gmail.com>
2023-10-12 11:53:01 +0900

    경로 수정

---
+++ src/components/Common/SideBar.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import styles from './SideBar.module.css';
+import { Link } from 'react-router-dom';
+
+
+function SideBar() {
+  return (
+    <div className={styles.sideBarBox}>
+      <Link to='/'>등록</Link>
+      <Link to='/'>오늘의 <br/>방사능</Link>
+      <Link to='/'>챗봇</Link>
+    </div>
+  )
+}
+
+export default SideBar;
---
+++ src/components/MainPage/Announce.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import styles from './Announce.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-bootstrap';
+
+function Announce() {
+  const carousel = {
+    height: '400px',
+    width: '60%',
+    margin: '0 auto',
+    borderRadius: '30px',
+    overflow: 'hidden'
+  }
+  return (           
+    <div style={carousel}>
+    <Carousel >
+      {/* 슬라이드 이미지 */}
+      <Carousel.Item id={styles.carousel}>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8_I0twC6CuVzdVxXirXhxPbCOZTrwBFFqjmeX4pTw&s"
+          alt="슬라이드 1"
+        />
+        <Carousel.Caption>
+          <h3>꽃소녀</h3>
+          <p>김철수 作</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item id={styles.carousel}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8_I0twC6CuVzdVxXirXhxPbCOZTrwBFFqjmeX4pTw&s" alt="슬라이드 2" />
+        <Carousel.Caption>
+          <h3>형형색색</h3>
+          <p>이숙자 作</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item id={styles.carousel}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8_I0twC6CuVzdVxXirXhxPbCOZTrwBFFqjmeX4pTw&s" alt="슬라이드 3" />
+        <Carousel.Caption>
+          <h3>분홍</h3>
+          <p>Robert Park 作</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item id={styles.carousel}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8_I0twC6CuVzdVxXirXhxPbCOZTrwBFFqjmeX4pTw&s" alt="슬라이드 4" />
+        <Carousel.Caption>
+          <h3>두꺼운 터치</h3>
+          <p>Thomas Lee 作</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  );
+}
+
+export default Announce;
