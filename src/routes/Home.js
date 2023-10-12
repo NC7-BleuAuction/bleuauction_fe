7083eeb060b510d20f950f471407fad13300e6c6
shin2012649 <shin2012649@gmail.com>
2023-10-12 11:56:53 +0900

    Home.js 경로 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import Announce from '../components/Announce';
-import Category from '../components/Category';
+import Announce from '../components/MainPage/Announce';
+import Category from '../components/MainPage/Category';
 import StoreListItem from '../components/Market/StoreListItem';
 import { Routes, Route } from 'react-router-dom';
 
