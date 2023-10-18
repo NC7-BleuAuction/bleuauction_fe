d338a622a385986d07c50c5e655c8fd66e204841
think8521 <think8521@naver.com>
2023-10-18 21:55:29 +0900

    로그인 정보 저장 수정, 홈 화면 수정

--- src/components/Auth/LoginPage.js
+++ src/components/Auth/LoginPage.js
@@ -39,6 +39,15 @@ function LoginPage() {
       login(formToJSON(data));
       localStorage.setItem('memberEmail', data.get('memberEmail'));
       localStorage.setItem('memberPwd', data.get('memberPwd'));
+      // sendAxiosRequest("/api/member/loginCheck", "GET", null,
+      // response => {
+      //   let repLoginUser = response.data.loginUser;
+      //   if (repLoginUser === null) {
+      //     window.location.href = '/main';
+      //   } else {
+      //     setLoginUser(repLoginUser);
+      //   }
+      // }, error => console.log(error));
     }, error => {
 
       console.log(error);
