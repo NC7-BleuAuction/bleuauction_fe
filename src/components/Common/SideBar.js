1c1e6f94682d8b05992d7c7ce537637ddc0f55f7
shin2012649 <shin2012649@gmail.com>
2023-10-19 10:37:45 +0900

    Clova Chatbot Api 프론트엔드2

--- src/components/Common/SideBar.js
+++ src/components/Common/SideBar.js
@@ -1,17 +1,24 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import styles from './SideBar.module.css';
 import { Link } from 'react-router-dom';
-
+import Chat from '../Chat/Chat';
 
 function SideBar() {
-  return (
-    <div className={styles.sideBarBox}>
-      <Link to='/'>등록</Link>
-      <Link to='/'>오늘의 <br/>방사능</Link>
-      <Link to='/chat'>챗봇</Link>
-    </div>
-  )
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    return (
+        <div className={styles.sideBarBox}>
+            <Link to="/">등록</Link>
+            <Link to="/">오늘의 <br />방사능</Link>
+            <Link to="#" onClick={toggleModal}>챗봇</Link>
+            {isModalOpen && <Chat closeModal={toggleModal} />}
+        </div>
+    );
 }
 
 export default SideBar;
