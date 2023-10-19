dad7e616d8244155e3ca3f5bf4c6596b59703f8a
itkw87 <itkw87@naver.com>
2023-10-19 14:28:29 +0900

    로그인 유지 코드 변경 후 공유용 commit

--- src/App.js
+++ src/App.js
@@ -12,19 +12,21 @@ import LoginPage from './components/Auth/LoginPage';
 import RegisterPage from './components/Auth/RegisterPage';
 import MenuList from './components/Menu/MenuList';
 import MenuRegisterationForm from './components/Menu/MenuRegisterationForm';
-import StoreList from './routes/StoreList';
+import StoreList from './components/Market/StoreList';
 import MarketDetailPage from './components/Market/MarketDetailPage';
 import ReviewForm from './components/Review/ReviewForm';
 import MyPage from './components/MyPage/MyPage';
 import UserEditPage from './components/MyPage/UserEditPage';
 import MyOrder from './components/MyPage/MyOrder';
 import StoreItemDailyPrice from './components/StoreItemDailyPrice/StoreItemDailyPrice';
-import UserProvider from  './components/Auth/UserContext';
+import UserProvider from './components/Auth/UserContext';
 import StoreRegisterPage from './components/MyPage/StoreRegisterPage';
 import AdminPage from './components/MyPage/AdminPage';
 import NoticeList from './components/Notice/NoticeList';
 import NoticeDetail from './components/Notice/NoticeDetail';
 import { useUser } from './components/Auth/UserContext';
+import StoreMyPage from './components/MyPage/StoreItemRegister'; // StoreMyPage 컴포넌트 파일의 경로에 따라 수정
+import StoreItemRegister from './components/MyPage/StoreItemRegister'; // StoreItemRegister 컴포넌트 파일의 경로에 따라 수정
 
 function App() {
   // const [hello, setHello] = useState('');
@@ -77,35 +79,35 @@ function RoutingComponent() {
 
   return (
     <>
-    <UserProvider>
-      {location.pathname !== "/login" && location.pathname !== "/register" && <Header />}
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" exact={true} element={<Home />} />
-        <Route path="/store/list" element={<StoreList />} />
-        <Route path="/api/test" element={<Test />} />
-        <Route path="/api/test2" element={<Test2 />} />
-        <Route path="/menulist" element={<MenuList />} />
-        <Route path="/reviewregister" element={<ReviewForm />} />
-        <Route path="/market/detail" element={<MarketDetailPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/storeRegister" element={<StoreRegisterPage />} />
-        <Route path="/useredit" element={<UserEditPage />} />
-        <Route path="/my-orders" element={<MyOrder />} />
+      <UserProvider>
+        {location.pathname !== "/login" && location.pathname !== "/register" && <Header />}
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/store/list" element={<StoreList />} />
+          <Route path="/api/test" element={<Test />} />
+          <Route path="/api/test2" element={<Test2 />} />
+          <Route path="/menulist" element={<MenuList />} />
+          <Route path="/reviewregister" element={<ReviewForm />} />
+          <Route path="/market/detail" element={<MarketDetailPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/storeRegister" element={<StoreRegisterPage />} />
+          <Route path="/useredit" element={<UserEditPage />} />
+          <Route path="/my-orders" element={<MyOrder />} />
           <Route path="/storemypage" element={<StoreMyPage />} />
           <Route path="/storeItemRegister" element={<StoreItemRegister />} />
 
           <Route path="/StoreItemDailyPrice" element={<StoreItemDailyPrice />} />
-        <Route path="/MenuRegisterationForm" element={<MenuRegisterationForm />} />
-        <Route path="/adminpage" element={<AdminPage />} />
-        <Route path="/notice/list" element={<NoticeList />} />
-        <Route path="/notice/detail/:noticeNo" element={<NoticeDetail />} />
+          <Route path="/MenuRegisterationForm" element={<MenuRegisterationForm />} />
+          <Route path="/adminpage" element={<AdminPage />} />
+          <Route path="/notice/list" element={<NoticeList />} />
+          <Route path="/notice/detail/:noticeNo" element={<NoticeDetail />} />
 
-      </Routes>
-      {location.pathname !== "/login" && location.pathname !== "/register" && <SideBar />}
-      {location.pathname !== "/login" && location.pathname !== "/register" && <Footer />}
-    </UserProvider>
+        </Routes>
+        {location.pathname !== "/login" && location.pathname !== "/register" && <SideBar />}
+        {location.pathname !== "/login" && location.pathname !== "/register" && <Footer />}
+      </UserProvider>
     </>
   );
 }
