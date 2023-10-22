96dab62ba53b970b6a15a1c6f3beb943145b2b24
seunghyun333 <shb2in33@naver.com>
2023-10-23 02:46:47 +0900

    상인 오더페이지 + 사진 불러오기

--- src/components/MyPage/StoreOrder.js
+++ src/components/MyPage/StoreOrder.js
@@ -2,32 +2,29 @@ import React, { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import styles from './MyOrderContent.module.css';
-import OrderMenuList from './OrderMenuList';
 
 
-function MyOrderContent() {
+
+function StoreOrder() {
 
   const [orders, setOrders] = useState([]);
 
   useEffect(()=>{
-    axios.get('/api/order')
+    axios.get('/api/store/order')
     .then(response => setOrders(response.data))
       .catch(error => console.log(error));
     console.log(orders);
   }, [])
 
-  // function onClick() {
-  //   console.log(orders);
-  // }
-
-  // const orders = orderlist;
 
   return (
     <>
+    <h1>주문확인</h1>
     {orders.map(order=>(
     <tr key={order.orderNo}>
+      
     <div>
-  
+    
     {/* <button onClick={onClick} >버튼</button> */}
     <div className={styles.order_box}>
       <div className={styles.order_store_title}>
@@ -38,18 +35,12 @@ function MyOrderContent() {
       
       <div className={styles.order_item_box}>
         <img src='/images/fresh.png'/>
-        <Link className="text-ellipsis" to={`/mypage/ordermenu/detail/${order && order.orderNo}`} style={{ color: '#909090' }}>
+        <Link className="text-ellipsis" to={`/order/detail/${order && order.orderNo}`} style={{ color: '#909090' }}>
         주문 상세보기
         </Link>
-        {/* {order.OrderMenus.map(orderMenu => (
-          <div key={orderMenu.orderMenuNo}>
-            <td>주문메뉴번호{orderMenu.orderMenuNo}</td>
-            <td>메뉴번호{orderMenu.menuNo}</td>
-            <td>주문수량{orderMenu.menuNo}</td>
-          </div>
-        ))}  */}       
-        
-      </div>
+
+      
+        </div>
       
       <div className={styles.order_result_box}>
       <td>총액: {order.orderPrice}</td>
@@ -67,6 +58,9 @@ function MyOrderContent() {
       <td>주소: {order.recipientAddr}</td>
       <br/>
       <td>상세주소: {order.recipientDetailAddr}</td>
+      
+      
+
 
 
       </div>
@@ -79,4 +73,4 @@ function MyOrderContent() {
   );
 }
 
-export default MyOrderContent;
+export default StoreOrder;
