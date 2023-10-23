4a0005875426d285fe627587d002bed3a2d054b3
shin2012649 <shin2012649@gmail.com>
2023-10-23 20:18:24 +0900

    사이드바 UI

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Announce from '../components/MainPage/Announce';
-import Category from '../components/MainPage/Category';
 import StoreListItem from '../components/Market/StoreListItem';
 import StoreHome from '../components/StoreHome';
 import { Routes, Route } from 'react-router-dom';
@@ -59,7 +58,6 @@ function Home() {
   return (
     <>
       <Announce />
-      <Category />
       {stores && (
         <>
           <StoreHome stores={stores} />
