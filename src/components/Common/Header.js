74a39c3cbff5e3bfdaaa3e27933a0e7defc04402
knh0113 <vickykwak@naver.com>
2023-10-23 17:57:23 +0900

    .

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,65 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { axios, formToJSON } from 'axios';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import { useUser } from '../Auth/UserContext';
-import { sendAxiosRequest } from '../utility/common';
-
-
+import { refreshTokenInvalid, getAccessToken, getLoginUserInfo, logout, accessTokenRefresh, isNullUndefinedOrEmpty, mainUrl, isTokenExpired } from '../utility/common';
+import jwtDecode from 'jwt-decode';
 
 
 function Header() {
+  const accessToken = sessionStorage.getItem('accessToken');
+  const refreshToken = localStorage.getItem('refreshToken');
+  const decodedAccToken = isTokenExpired(accessToken) ? null : jwtDecode(accessToken);
+  const decodedRefToken = isTokenExpired(refreshToken) ? null : jwtDecode(refreshToken);
+  const currentURL = window.location.href;
+  const [tokenMember, setTokenMember] = useState(null);
 
-  // const {user, logout} = useUser();
-  let [loginUser, setLoginUser] = useState(null);
+  // console.log('Header.js => accessToken 디코딩값 : ', decodedAccToken);
+  // console.log('Header.js => refreshToken 디코딩값 : ', decodedRefToken);
+  // console.log('(currentURL.replace(mainUrl)', currentURL.replace(mainUrl, ''));
 
-  // useEffect (()=>{
-  //   sendAxiosRequest("/api/member/loginCheck", "GET", null,
-  //   response => {
-  //     let repLoginUser = response.data.loginUser;
-  //     if (repLoginUser !== null) {
-  //       setLoginUser(repLoginUser);
-  //     }
-  //   }, error => console.log(error));}, []
-  // )
-    // let [loginUser, setLoginUser] = useState(null);
+  let loginInit
+
+
   useEffect(() => {
-    if (loginUser == null) {
-      sendAxiosRequest("/api/member/loginCheck", "GET", null,
-        response => {
-          let repLoginUser = response.data.loginUser;
-          if (repLoginUser != null) {
-            setLoginUser(repLoginUser);
-          }
-        }, error => console.log(error));
+    if (isNullUndefinedOrEmpty(accessToken)) { // 로그인 중이면
+
+      if (!decodedAccToken && decodedRefToken) { // accessToken은 유효하지 않으면서 refreshToken이 유효한 경우
+        console.log('Header.js => 엑세스 토큰 만료에 따른 재발급!');
+        if (currentURL.replace(mainUrl, '') !== '/') {
+          console.log('accessTokenRefresh(): ', accessTokenRefresh()); // accessToken 재발급
+        }
+      } else if (!decodedAccToken && !decodedRefToken) { // 둘다 유효하지 않은 경우
+        console.log('Header.js => accessToken (X) && refreshToken (X)');
+        if (currentURL.replace(mainUrl, '') !== '/') {
+          refreshTokenInvalid();
+        }
+      } else { // 둘다 유효하거나 accessToken만 유효한 경우
+        console.log('Header.js => accessToken만 유효 OR accessToken과 refreshToken 모두 유효');
+        setTokenMember(decodedAccToken);
+      }
     }
-  }, [loginUser]
-  )
-
-  // const loginState = (loginUser == null) ?
-  //   <Link to='/login'>로그인</Link> :
-  //   <Link to='/mypage'>{loginUser.memberEmail}님</Link>;
-
-  const logoutCall = () => {
-      sendAxiosRequest("/api/member/logout", "GET", null,
-          response => {
-              const message = response.data.message;
-              if (message != null && message == 'Logout successful') {
-                  alert('로그아웃에 성공하였습니다!');
-                  setLoginUser(null);
-              }
-          }, error => console.log(error));
-  }
-  const onClick = () => {
-    // logout();
-    logoutCall();
-    localStorage.removeItem('memberEmail');
-    localStorage.removeItem('memberPwd');
-  }
-
-  // const logoutState = (user === null) ?
-  // <Link to='/register'>회원가입</Link> :
-  // <Link to='/' onClick={onClick}>로그아웃</Link>;
+  }, []);
 
   return (
     <>
@@ -70,24 +51,29 @@ function Header() {
             <input className={styles.headerSearchBox} type='text' placeholder='검색어를 입력하세요.'></input>
             <button id={styles.searchBtn} type="submit"></button>
           </form>
-          {loginUser == null ? (<Link to='/login'>로그인</Link>) : (<Link to='/mypage'>{loginUser.memberName}님 환영합니다!</Link>)}
-          {loginUser == null ? (<Link to='/register'>회원가입</Link>) : (<Link onClick={logoutCall}>로그아웃</Link>)}
-          <Link to="/api/test" >test</Link>
-          <Link to='/market/detail'>가게1</Link>
-          <Link to='/my-orders'>주문상세</Link>
-
+          {isNullUndefinedOrEmpty(tokenMember) ? (
+            <>
+              <Link to='/mypage'>{tokenMember.memberName}님 환영합니다!</Link>
+              <Link onClick={logout}>로그아웃</Link>
+            </>
+          ) : (
+            <>
+              <Link to='/login'>로그인</Link>
+              <Link to='/register'>회원가입</Link>
+            </>
+          )}
         </div>
 
 
-          <div className={styles.headerBottom}>
-              <Link to='/'>추천</Link>
-              <Link to='/store/list'>시장</Link>
-              <Link to='/StoreItemDailyPrice'>시세</Link>
-              <Link to='/notice/list'>공지사항</Link>
-          </div>
-          <hr></hr>
+        <div className={styles.headerBottom}>
+          <Link to='/'>추천</Link>
+          <Link to='/store/list'>시장</Link>
+          <Link to='/StoreItemDailyPrice'>시세</Link>
+          <Link to='/notice/list'>공지사항</Link>
+        </div>
+        <hr></hr>
       </div>
-        <div className={styles.hidden_block}></div>
+      <div className={styles.hidden_block}></div>
     </>
   )
 }
