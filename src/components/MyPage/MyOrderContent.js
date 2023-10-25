d4af275cdecf2b807daa998b936a81cb0a6d0793
knh0113 <vickykwak@naver.com>
2023-10-25 21:37:38 +0900

    마이오더

--- src/components/MyPage/MyOrderContent.js
+++ src/components/MyPage/MyOrderContent.js
@@ -31,61 +31,98 @@ function MyOrderContent() {
 
   // const orders = orderlist;
 
-  return (
-    <>
-    {orders.map(order=>(
-    <tr key={order.orderNo}>
-    <div>
+//   return (
+//     <>
+//     {orders.map(order=>(
+//     <tr key={order.orderNo}>
+//     <div>
   
-    {/* <button onClick={onClick} >버튼</button> */}
-    <div className={styles.order_box}>
-      <div className={styles.order_store_title}>
-      <td>주문 번호 : {order.orderNo}</td>
-      <br/>
-      <td>주문일: {new Date(order.regDatetime).toLocaleDateString()}</td>
-      </div>
+//     {/* <button onClick={onClick} >버튼</button> */}
+//     <div className={styles.order_box}>
+//       <div className={styles.order_store_title}>
+//       <td>주문 번호 : {order.orderNo}</td>
+//       <br/>
+//       <td>주문일: {new Date(order.regDatetime).toLocaleDateString()}</td>
+//       </div>
       
-      <div className={styles.order_item_box}>
-        <img src='/images/fresh.png'/>
-        <Link className="text-ellipsis" to={`/mypage/ordermenu/detail/${order && order.orderNo}`} style={{ color: '#909090' }}>
-        주문 상세보기
-        </Link>
-        {/* {order.OrderMenus.map(orderMenu => (
-          <div key={orderMenu.orderMenuNo}>
-            <td>주문메뉴번호{orderMenu.orderMenuNo}</td>
-            <td>메뉴번호{orderMenu.menuNo}</td>
-            <td>주문수량{orderMenu.menuNo}</td>
-          </div>
-        ))}  */}       
+//       <div className={styles.order_item_box}>
+//         <img src='/images/fresh.png'/>
+//         <Link className="text-ellipsis" to={`/mypage/ordermenu/detail/${order && order.orderNo}`} style={{ color: '#909090' }}>
+//         주문 상세보기
+//         </Link>
+//         {/* {order.OrderMenus.map(orderMenu => (
+//           <div key={orderMenu.orderMenuNo}>
+//             <td>주문메뉴번호{orderMenu.orderMenuNo}</td>
+//             <td>메뉴번호{orderMenu.menuNo}</td>
+//             <td>주문수량{orderMenu.menuNo}</td>
+//           </div>
+//         ))}  */}       
         
-      </div>
+//       </div>
       
-      <div className={styles.order_result_box}>
-      <td>총액: {order.orderPrice}</td>
-      <br/>
-      <td>주문타입: {order.orderType === 'Q' ? '퀵배송' : order.orderType === 'T' ? '포장' : '기타'}</td>
-      <br/>
-      <td>요청사항: {order.orderRequest}</td>
-      <br/>
-      <td>핸드폰 번호: {order.recipientPhone}</td>
-      <br/>
-      <td>수령자: {order.recipientName}</td>
-      <br/>
-      <td>우편번호: {order.recipientZipcode}</td>
-      <br/>
-      <td>주소: {order.recipientAddr}</td>
-      <br/>
-      <td>상세주소: {order.recipientDetailAddr}</td>
+//       <div className={styles.order_result_box}>
+//       <td>총액: {order.orderPrice}</td>
+//       <br/>
+//       <td>주문타입: {order.orderType === 'Q' ? '퀵배송' : order.orderType === 'T' ? '포장' : '기타'}</td>
+//       <br/>
+//       <td>요청사항: {order.orderRequest}</td>
+//       <br/>
+//       <td>핸드폰 번호: {order.recipientPhone}</td>
+//       <br/>
+//       <td>수령자: {order.recipientName}</td>
+//       <br/>
+//       <td>우편번호: {order.recipientZipcode}</td>
+//       <br/>
+//       <td>주소: {order.recipientAddr}</td>
+//       <br/>
+//       <td>상세주소: {order.recipientDetailAddr}</td>
 
 
-      </div>
-      </div>
+//       </div>
+//       </div>
    
-    </div>
-    </tr>
-  ))}
-       </>
-  );
+//     </div>
+//     </tr>
+//   ))}
+//        </>
+//   );
+// }
+
+return (
+  <div className={styles.orderListContainer}>
+    {orders.map((order) => (
+      <div key={order.orderNo} className={styles.orderCard}>
+        <div className={styles.orderHeader}>
+          <p>주문 번호 : {order.orderNo}</p>
+          <Link to={`/mypage/ordermenu/detail/${order.orderNo}`} className={styles.detailLink}>
+              주문 상세보기
+            </Link>       
+         </div>
+        
+        <div className={styles.orderContent}>
+          <div className={styles.orderImage}>
+            <img src='/images/fresh.png' alt='Order' />
+          </div>
+          <div className={styles.orderDetails}>
+            {/* Additional order details can be structured here */}
+            <p>총액: {order.orderPrice}</p>
+            <p>주문타입: {order.orderType === 'Q' ? '퀵배송' : order.orderType === 'T' ? '포장' : '기타'}</p>
+            <p>요청사항: {order.orderRequest}</p>
+            <p>핸드폰 번호: {order.recipientPhone}</p>
+            <p>수령자: {order.recipientName}</p>
+            <p>우편번호: {order.recipientZipcode}</p>
+            <p>주소: {order.recipientAddr}</p>
+            <p>상세주소: {order.recipientDetailAddr}</p>
+            <p>주문일: {new Date(order.regDatetime).toLocaleDateString()}</p>
+            {/* ... other order-related information ... */}
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 }
+
+
 
 export default MyOrderContent;
